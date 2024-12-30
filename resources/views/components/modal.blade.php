7b91ff443faf375185efa9c6760870916974e5fd
Edyelgue Carneiro <edyelgue12@gmai.com>
2024-12-30 12:43:15 -0300

    Layout da modal atualizado

--- resources/views/components/modal.blade.php
+++ resources/views/components/modal.blade.php
@@ -1,14 +1,15 @@
 <div class="flex justify-items-end">
     <!-- You can open the modal using ID.showModal() method -->
-    <button class="btn bg-red-900 btn-ghost hover:bg-red-950" onclick="my_modal_4.showModal()">
+    <button class="p-2 rounded-lg bg-red-800 btn-ghost hover:bg-red-950" onclick="my_modal_4.showModal()">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="white" class="size-4">
             <path
                 d="M14 2a1 1 0 0 0-1-1H3a1 1 0 0 0-1 1v2.172a2 2 0 0 0 .586 1.414l2.828 2.828A2 2 0 0 1 6 9.828v4.363a.5.5 0 0 0 .724.447l2.17-1.085A2 2 0 0 0 10 11.763V9.829a2 2 0 0 1 .586-1.414l2.828-2.828A2 2 0 0 0 14 4.172V2Z" />
         </svg>
     </button>
+    <h2 class="font-bold text-md md:text-lg items-center flex px-2">Filtros</h2>
     <dialog id="my_modal_4" class="modal">
         <div class="modal-box w-11/12 max-w-5xl">
-            <h3 class="text-lg font-bold">Filtros</h3>
+            <h3 class="text-md md:text-lg font-bold">Filtros</h3>
             <div class="flex flex-col">
                 {{ $slot }}
             </div>
--- resources/views/components/title-primary.blade.php
+++ resources/views/components/title-primary.blade.php
@@ -1,1 +1,1 @@
-<h1 class="sm:text-4xl text-3xl font-bold title-font mb-2 text-gray-700 text-center py-4">{{ $slot }}</h1>
+<h1 class="text-xl sm:text-2xl md:text-4xl font-bold title-font mb-2 text-gray-700 text-center py-4">{{ $slot }}</h1>
